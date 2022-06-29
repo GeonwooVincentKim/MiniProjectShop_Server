94df9a5d707d68dbe9f08570235ab71bb100952c
GeonwooVincentKim <kdsnop@gmail.com>
2022-06-29 10:30:39 +0900

    [Update] - Check App.js works

--- app.js
+++ app.js
@@ -3,20 +3,34 @@ const app = express();
 const session = require('express-session');
 const fs = require('fs');
 
-app.use(
-    session({
-        secret: '',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: false,
-            maxAge: 1000 * 60 * 60,
-        },
-    })
-);
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.use(
-    express.json({
-        limit: '50mb',
-    })
-);
+// app.use(
+//     session({
+//         secret: '',
+//         resave: false,
+//         saveUninitialized: false,
+//         cookie: {
+//             secure: false,
+//             maxAge: 1000 * 60 * 60,
+//         },
+//     })
+// );
+
+// app.use(
+//     express.json({
+//         limit: '50mb',
+//     })
+// );
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
